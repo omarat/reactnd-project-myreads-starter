7644ca8e9450b3bc5fc8895316213685968ff0d7
Zepeda Beltran <szepeda@mac-szepeda.local>
2017-10-30 00:05:04 -0400

    feature: Add Book and Booklist component, support moving books around

---
+++ src/Book.js
@@ -0,0 +1,31 @@
+import React,{Component} from 'react'
+
+
+class Book extends Component{
+  render(){
+    const {title,author,coverURL,id,shelf}= this.props
+
+    return(
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover"
+            style={{ width: 128, height: 193, backgroundImage: `url(${coverURL})` }}>
+
+            </div>
+          <div className="book-shelf-changer">
+            <select onChange={(e)=>this.props.moveHandler(id,e.target.value)} value={shelf}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">{author}</div>
+      </div>
+    )
+  }
+}
+export default Book
